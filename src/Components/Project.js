0b030ec8532fece86034d6d930886ab8619d1f25
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-01-06 11:56:18 +0600

    Project Component Complete

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -23,6 +23,35 @@ export default function Project() {
                 <a href="https://wordcounter25.netlify.app/" target="_blank" rel="noopener noreferrer">Live Netlify</a>
             </div>
         </div>
+        <div className='projectItem'>
+            <h3 className='projectH3'>Certificate Management System</h3>
+            <p>A website to automate the certificate related task for both Student and Administration of SSC and HSC.</p>
+            <p><span className='tech'>Technologies Used:</span> HTML, CSS, JavaScript, PHP, MySQL</p>
+            <div className='buttons'>
+                <a href="https://github.com/Saj1d-prompt/CertificateManagementSystem" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+            </div>
+        </div>
+        <div className='projectItem'>
+            <h3 className='projectH3'>Complaint Management System</h3>
+            <p>A website to automate the Complaint related task for the User and Administration and Staff.</p>
+            <p><span className='tech'>Technologies Used:</span> HTML, CSS, PHP, MySQL</p>
+            <div className='buttons'>
+                <a href="https://github.com/Saj1d-prompt/ComplaintManagementSystem" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+            </div>
+        </div>
+        <div className='projectItem'>
+            <h3 className='projectH3'>Birth Record Communication System</h3>
+            <p>A website to automate the Birth Record and automatically apply for a birth certificate and also to keep track of the newborns vaccination reminders.</p>
+            <p><span className='tech'>Technologies Used:</span> HTML, CSS, JavaScript, PHP, MySQL</p>
+            <div className='buttons'>
+                <a href="https://github.com/Saj1d-prompt/BirthRecordCommunicationSystem" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+            </div>
+        </div>
+        To view More Projects Please Click Below to visit my GitHub account:
+        <div className='buttons'>
+                <a href="https://github.com/Saj1d-prompt" target="_blank" rel='noopener noreferrer' className='moreProjects'>More</a>
+        </div>
+        
     </div>
   )
 }
